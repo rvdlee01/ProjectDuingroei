31adb5c025c19898ca48553f0c4a68c52d4397ab
rvdlee01 <rvdlee01@gmail.com>
2021-05-12 11:52:58 +0200

    added matplotlib toolbar

--- testgui.py
+++ testgui.py
@@ -64,5 +64,9 @@ class PageOne(tk.Frame):
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand= True)
 
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
 app = DuinGroeiApp()
 app.mainloop()

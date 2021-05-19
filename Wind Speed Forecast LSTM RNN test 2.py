7b2e879a736824cf82b0d4cc612da9b1dbde4f67
wingyeechung <58391243+wingyeechung@users.noreply.github.com>
2021-05-20 00:41:51 +0200

    Fixed variable names and removed unneccessary functions

--- Wind Speed Forecast LSTM RNN test 2.py
+++ Wind Speed Forecast LSTM RNN test 2.py
@@ -3,15 +3,10 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import calendar
-from keras.models import Sequential
-from keras.models import Model
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
+from keras.models import Sequential, Model
+from keras.layers import Dense, LSTM, Dropout
 from keras.preprocessing.sequence import TimeseriesGenerator
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
@@ -20,12 +15,6 @@ def create_dates(start,days):
     v = pd.date_range(start=start, periods=days+1, freq='D', closed='right')
     one_year_forecast = pd.DataFrame(index=v) 
     return one_year_forecast
-
-# get values, station name and drop null values-----------------DROP
-def get_value_name(all_station_temp,i):
-    station_value = all_station_temp[[all_station_temp.columns[i]]].dropna()
-    station_name = all_station_temp.columns[i]
-    return station_value, station_name
 
 # train-test split for a user input ratio
 def train_test_split(value, name, ratio):
@@ -39,33 +28,34 @@ def train_test_split(value, name, ratio):
     return train, test, split_row     
 
 # data transformation
-def data_transformation(train_tract1,test_tract1):
+def data_transformation(x_train,x_test):
     scaler = MinMaxScaler()
-    train_tract1_scaled = scaler.fit_transform(train_tract1)
-    test_tract1_scaled = scaler.fit_transform(test_tract1)          
-    train_tract1_scaled_df = pd.DataFrame(train_tract1_scaled, index = train_tract1.index, columns=[train_tract1.columns[0]])
-    test_tract1_scaled_df = pd.DataFrame(test_tract1_scaled,
-                                         index = test_tract1.index, columns=[test_tract1.columns[0]])
-    return train_tract1_scaled_df, test_tract1_scaled_df, scaler  
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_test_scaled = scaler.fit_transform(x_test)
+
+    # Create new dataframe for scaled data
+    x_train_scaled_df = pd.DataFrame(x_train_scaled, index = x_train.index, columns=[x_train.columns[0]])
+    x_test_scaled_df = pd.DataFrame(x_test_scaled, index = x_test.index, columns=[x_test.columns[0]])
+
+    return x_train_scaled_df, x_test_scaled_df, scaler  
 
 # feature builder - This section creates feature set with lag number of predictors--Creating features using lagged data
 def timeseries_feature_builder(df, lag):
     df_copy = df.copy()
     for i in range(1,lag):
-        df_copy['lag'+str(i)] = df.shift(i) 
+        df_copy['lag'+str(i)] = df.shift(i)
     return df_copy
-    df_copy = df.copy()
 
 # preprocessing -- drop null values and make arrays 
-def make_arrays(train_tract1,test_tract1):
-    X_train_tract1_array = train_tract1.dropna().drop(train_tract1.columns[0], axis=1).values
-    y_train_tract1_array = train_tract1.dropna()[train_tract1.columns[0]].values
-    X_test_tract1_array = test_tract1.dropna().drop(test_tract1.columns[0], axis=1).values
-    y_test_tract1_array = test_tract1.dropna()[test_tract1.columns[0]].values    
-    return X_train_tract1_array, y_train_tract1_array, X_test_tract1_array, y_test_tract1_array
+def make_arrays(x_train,x_test):
+    x_train_array = x_train.dropna().drop(x_train.columns[0], axis=1).values
+    y_train_array = x_train.dropna()[x_train.columns[0]].values
+    x_test_array = x_test.dropna().drop(x_test.columns[0], axis=1).values
+    y_test_array = x_test.dropna()[x_test.columns[0]].values
+    return x_train_array, y_train_array, x_test_array, y_test_array
 
 # Define LSTM model
-def lstm_model(units, trainX, testX, y_train_tract1_array, y_test_tract1_array):
+def lstm_model(units, trainX, testX, y_train_array, y_test_array):
     model = Sequential()
     model.add(LSTM(units,return_sequences=True, input_shape=(trainX.shape[1],trainX.shape[2]),kernel_initializer='lecun_uniform'))
     model.add(Dropout(0.2))    
@@ -76,23 +66,23 @@ def lstm_model(units, trainX, testX, y_train_tract1_array, y_test_tract1_array):
     model.add(Dense(1))        
     model.compile(optimizer='adam', loss='mean_squared_error')
     
-    model.fit(trainX, y_train_tract1_array, batch_size=120, epochs=100, validation_data=(testX, y_test_tract1_array), verbose=0)
+    model.fit(trainX, y_train_array, batch_size=120, epochs=100, validation_data=(testX, y_test_array), verbose=0)
     return model
 
 # validation result 
-def valid_result(model, testX, y_test_tract1_array, scaler, station_value, split_row, lag):    
+def valid_result(model, testX, y_test_array, scaler, all_windspeed, split_row, lag):    
     testPredict = model.predict(testX)
-    rSquare_test = r2_score(y_test_tract1_array, testPredict)
+    rSquare_test = r2_score(y_test_array, testPredict)
     print('Test R-squared is: %f'%rSquare_test)    
     testPredict = scaler.inverse_transform(testPredict)        
-    new_test_tract1 = station_value.iloc[split_row:]       
-    test_tract1_pred = new_test_tract1.iloc[lag:].copy()
-    test_tract1_pred['Forecast'] = testPredict
-    return test_tract1_pred 
+    new_test = all_windspeed.iloc[split_row:]       
+    x_test_pred = new_test.iloc[lag:].copy()
+    x_test_pred['Forecast'] = testPredict
+    return x_test_pred 
 
 # multi step future forecast for next days number of days. 
 def forecast(model, testX, test_tract1, lag, scaler, days):
-    seven_days = []
+    one_year = []
     new0 = testX[-1]        
     last = test_tract1.iloc[-1]
     new_predict = last[0]        
@@ -101,7 +91,7 @@ def forecast(model, testX, test_tract1, lag, scaler, days):
     new_array_reshape = np.reshape(new_array, (-1,1,lag))       
     new_predict = model.predict(new_array_reshape)
     temp_predict = scaler.inverse_transform(new_predict) 
-    seven_days.append(temp_predict[0][0].round(2))
+    one_year.append(temp_predict[0][0].round(2))
     
     for i in range(1,days):
         new_array = np.insert(new_array, 0, new_predict)             
@@ -109,46 +99,49 @@ def forecast(model, testX, test_tract1, lag, scaler, days):
         new_array_reshape = np.reshape(new_array, (-1,1,lag))            
         new_predict = model.predict(new_array_reshape)
         temp_predict = scaler.inverse_transform(new_predict) 
-        seven_days.append(temp_predict[0][0].round(2))
-    return seven_days     
+        one_year.append(temp_predict[0][0].round(2))
+    return one_year     
 
 def windspeed_lstm(all_windspeed, lag, days):    
-    
+
+    # Create list of dates
     one_year_forecast_lstm = create_dates('2021-05-17',days)
-    print(one_year_forecast_lstm)
-    print(len(all_windspeed.columns))
-    for i in range(len(all_windspeed.columns)):
+    
+    # Preprocessing
+    windspeed_df = all_windspeed
+    column_name = all_windspeed.columns[0] # Column name is FHX
+
+    # Split data into 90% training data and 10% test data
+    x_train, x_test, split_row = train_test_split(windspeed_df, column_name, 0.90)
+
+    # Scale data
+    x_train_scaled_df, x_test_scaled_df, scaler = data_transformation(x_train,x_test)
+
+    #???????????????????
+    x_train = timeseries_feature_builder(x_train_scaled_df, lag+1) 
+    x_test = timeseries_feature_builder(x_test_scaled_df, lag+1)
+
+    # Create arrays
+    x_train_arr, y_train_arr, x_test_arr, y_test_arr = make_arrays(x_train,x_test)
         
-        # preprocessing
-        station_value, station_name = get_value_name(all_windspeed,i)
+    trainX = np.reshape(x_train_arr, (x_train_arr.shape[0],1,x_train_arr.shape[1]))
+    testX = np.reshape(x_test_arr, (x_test_arr.shape[0],1,x_test_arr.shape[1]))                
         
-        train_tract1, test_tract1, split_row = train_test_split(station_value, station_name, 0.90)
+    # LSTM modelling & forecast
+    model = lstm_model(30, trainX, testX, y_train_arr, y_test_arr)
+    
+    x_test_pred = valid_result(model, testX, y_test_arr, scaler, windspeed_df, split_row, lag)        
+    one_year = forecast(model, testX, x_test, lag, scaler, days)       
+    one_year_forecast_lstm[column_name] = np.array(one_year)       
         
-        train_tract1_scaled_df, test_tract1_scaled_df, scaler = data_transformation(train_tract1,test_tract1)
-        
-        train_tract1 = timeseries_feature_builder(train_tract1_scaled_df, lag+1) 
-        test_tract1 = timeseries_feature_builder(test_tract1_scaled_df, lag+1)
-        
-        X_train_tract1_array, y_train_tract1_array, X_test_tract1_array, y_test_tract1_array = make_arrays(train_tract1, 
-                                                                                                           test_tract1)
-        
-        trainX = np.reshape(X_train_tract1_array, (X_train_tract1_array.shape[0],1,X_train_tract1_array.shape[1]))
-        testX = np.reshape(X_test_tract1_array, (X_test_tract1_array.shape[0],1,X_test_tract1_array.shape[1]))                
-        
-        # LSTM modelling & forecast
-        model = lstm_model(30, trainX, testX, y_train_tract1_array, y_test_tract1_array)             
-        test_tract1_pred = valid_result(model, testX, y_test_tract1_array, scaler, station_value, split_row, lag)        
-        seven_days = forecast(model, testX, test_tract1, lag, scaler, days)       
-        one_year_forecast_lstm[station_name] = np.array(seven_days)       
-        
-        # plot result
-        plt.plot(test_tract1_pred)        
-        plt.plot(one_year_forecast_lstm[station_name], color='red', label='forecast')         
-        plt.ylabel('Wind Speed')
-        plt.legend(loc='upper right')
-        plt.show()        
-        
-    return(one_year_forecast_lstm)
+    # Plot result
+    plt.plot(x_test_pred)        
+    plt.plot(one_year_forecast_lstm[column_name], color='red', label='forecast')         
+    plt.ylabel('Windspeed in 0.1 m/s')
+    plt.legend(loc='upper right')
+    plt.show()        
+
+    return one_year_forecast_lstm
 
 def readCSV(filename):
     df = pd.read_csv(filename, skipinitialspace=True)

f043333af4875ca8b5aca9fe028da2c80ac28f02
rvdlee01 <rvdlee01@gmail.com>
2021-05-28 09:35:43 +0200

    Update testgui.py

--- testgui.py
+++ testgui.py
@@ -7,9 +7,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 from matplotlib.figure import Figure
 
 from tkinter import *
-
 import tkinter as tk
-from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 
@@ -35,6 +33,7 @@ def load_Data(self,csv_data):
     return None
 
 
+
 def getCsvFile(self):
     global filename
     filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("Csv files", "*.csv"),("xlsx files","*.xlsx")))
@@ -44,21 +43,36 @@ def getCsvFile(self):
         self.tv1.delete(*self.tv1.get_children()) #Empties rows and columns in csvTable
         load_Data(self,filename)
         
-    filename = filedialog.askopenfilename()
     
 
-class DuinGroeiApp(self, tk.Tk):
-    tk.Tk.iconbitmap(self, default="dune_icon.ico")
-    tk.Tk.wm_title(self, "DuinHoogte")
-    tk.Tk.wm_geometry(self, "1600x900")
+class DuinGroeiApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-    container = tk.Frame(self)
-    container.pack(side="top", fill="both", expand=True)
-    frame = self.frames[cont]
-    frame.tkraise()
+        tk.Tk.iconbitmap(self, default="dune_icon.ico")
+        tk.Tk.wm_title(self, "DuinHoogte")
+        tk.Tk.wm_geometry(self, "1600x900")
 
-def get_page(self, page_class): # returns reference to a page
-    return self.frames[page_class]
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames = {}
+
+        for F in (StartPage, PageOne):
+            frame = F(container, self)
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+    def get_page(self, page_class): # returns reference to a page
+        return self.frames[page_class]
 
 class StartPage(tk.Frame):
     global button1
@@ -66,17 +80,23 @@ class StartPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         myFont = font.Font(family = 'Helvetica', size = 30)
-          
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
-        dwnd = PhotoImage(file='download.png')
-        Button(ws, image=dwnd, command=None).pack(pady=10)
+        
+        select = PhotoImage(file='download.png')
+
         self.button1 = tk.Button(self,state = DISABLED, text="Go to graph page",
                             command=lambda: [controller.show_frame(PageOne),self.csvTable.place_forget()], font = myFont)
-        self.button2 = tk.Button(self, text="Select csv file",
-                            command=lambda: getCsvFile(self), font = myFont)
+        self.button2 = tk.Button(self, image=select,
+                            command=lambda: getCsvFile(self), font = myFont, borderwidth = 0)
+        self.button2.image = select
         self.button1.pack(padx=50,pady=50)
         self.button2.pack()
+        
+        
+
+        
+        
         
         # Frame for Treeview
         self.csvTable = tk.LabelFrame(text ="CSV data")
@@ -95,13 +115,6 @@ class StartPage(tk.Frame):
 
         # self.csvTable.destroy()
 
-         
-        button1 = ttk.Button(self, text="Go to graph page", 
-                            command=lambda: controller.show_frame(PageOne))
-        button2 = ttk.Button(self, text="Select csv file", 
-                            command=lambda: getCsvFile())
-        button1.pack()
-        button2.pack()
 
 class PageOne(tk.Frame):
     def __init__(self, parent, controller):
@@ -110,20 +123,19 @@ class PageOne(tk.Frame):
         start_page = self.controller.get_page(StartPage) # Access variables from another class
         label = tk.Label(self, text="Graph page", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
-
         button1 = ttk.Button(self, text="Home",
                             command=lambda: [controller.show_frame(StartPage), start_page.csvTable.place(height=450,width=800,x = 400, y = 350)])
         button1.pack()
 
-        f = Figure(figsize=(5,5), dpi=100)
+        f = Figure(figsize=(10,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,7,3,4,5,6,9])
 
+        canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand= True)
 
         
-        toolbar = NavigationToolbar2Tk(canvas, self)
-        toolbar.update()
-        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 app = DuinGroeiApp()
 app.mainloop()
